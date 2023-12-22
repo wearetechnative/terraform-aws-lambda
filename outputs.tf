46e0f7db6478a97867e343e88c61007280a15f7e
Andrew Ng <andrew@technative.eu>
2023-12-22 11:01:01 +0100

    Introduced invoke ARN output for API gateway use

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "lambda_function_arn" {
 output "lambda_function_name" {
   value = aws_lambda_function.this.function_name
 }
+
+output "lambda_function_invoke_arn" {
+  value = aws_lambda_function.this.invoke_arn
+}
